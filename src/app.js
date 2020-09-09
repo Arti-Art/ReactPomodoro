c1a10eb7beaf09193318f5cf83f29b10027d08a0
artiart <art.kouznetsov@gmail.com>
2020-09-09 16:28:30 +0200

    It's Aliiive!

--- src/app.js
+++ src/app.js
@@ -5,4 +5,8 @@
 .* started on 09/09/2020
 */
 
-console.warn("I'm Ready");
+import React from "react";
+import ReactDOM from "react-dom";
+import RootComponent from "./components/root";
+
+ReactDOM.render(<RootComponent />, document.querySelector("#app"));
---
+++ src/components/root.js
@@ -0,0 +1,12 @@
+/*
+.* React Test App
+.* /src/components/root.js - Root Component
+.* coded by Arti-Art
+.* started on 09/09/2020
+*/
+
+import React from "react";
+
+const RootComponent = () => <p>{"Hello World"}</p>;
+
+export default RootComponent;
